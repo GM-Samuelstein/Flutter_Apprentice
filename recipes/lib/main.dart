f91664376d5c85fef1ea56b73aa9466c68fb226a
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-04-22 03:35:20 +0100

    added recipe card

--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -49,10 +49,20 @@ class _MyHomePageState extends State<MyHomePage> {
             itemCount: Recipe.samples.length,
             itemBuilder: (BuildContext context, int index) {
               //TODO: Update to return Recipe card
-              return Text(Recipe.samples[index].label);
+              return buildRecipeCard(Recipe.samples[index]);
             }),
       ),
     );
   }
-  // TODO: Add buildRecipeCard() here
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl),),
+          Text(recipe.label),
+        ],
+      ),
+    );
+  }
 }
