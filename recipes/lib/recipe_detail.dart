23b7b1cbb093b332a47f988a2c18c870337c9d9a
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-04-22 15:45:36 +0100

    updated the recipe with slider

--- recipes/lib/recipe_detail.dart
+++ recipes/lib/recipe_detail.dart
@@ -15,17 +15,19 @@ class RecipeDetail extends StatefulWidget {
 
 // ADD: _RecipeDetailState here
 class _RecipeDetailState extends State<RecipeDetail> {
-  // TODO: Add _sliderVal here
+  int _sliderVal = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text(widget.recipe.label),),
+        title: Center(
+          child: Text(widget.recipe.label),
+        ),
       ),
       body: SafeArea(
         child: Column(
-          children: <Widget> [
+          children: <Widget>[
             SizedBox(
               height: 300,
               width: double.infinity,
@@ -46,15 +48,26 @@ class _RecipeDetailState extends State<RecipeDetail> {
                 itemCount: widget.recipe.ingredients.length,
                 itemBuilder: (BuildContext context, int index) {
                   final ingredient = widget.recipe.ingredients[index];
-                  // TODO: Add ingredient.quantity
-                  return Text(
-                    '${ingredient.quantity} ${ingredient.measure}'
-                    '${ingredient.name}'
-                  );
+
+                  return Text('${_sliderVal * ingredient.quantity} ' 
+                  '${ingredient.measure} ${ingredient.name}');
                 },
               ),
             ),
-            // TODO: Add Slider() here
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 9,
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              value: _sliderVal.toDouble(),
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
           ],
         ),
       ),
