2db6691d14d2dade6ed5c8e9bb87807467fa983b
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2023-04-22 13:37:59 +0100

    created recipe detail screen

--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -22,25 +23,23 @@ class RecipeApp extends StatelessWidget {
           secondary: Colors.black,
         ),
       ),
-      home: const MyHomePage(title: 'Recipe Calculator'),
+      home: const HomePage(title: 'Recipe Calculator'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -51,22 +50,32 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
             itemCount: Recipe.samples.length,
             itemBuilder: (BuildContext context, int index) {
-              //TODO: Add GestureDetector
-              return buildRecipeCard(Recipe.samples[index]);
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return RecipeDetail(recipe: Recipe.samples[index]);
+                      },
+                    ),
+                  );
+                },
+                child: buildRecipeCard(Recipe.samples[index]),
+              );
             }),
       ),
     );
   }
 
   Widget buildRecipeCard(Recipe recipe) {
-
     return Card(
       elevation: 200.0,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
       child: Padding(
-        padding:const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           children: <Widget>[
             Image(image: AssetImage(recipe.imageUrl)),
@@ -80,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 fontWeight: FontWeight.w600,
                 fontFamily: 'Palatino',
               ),
-              ),
+            ),
           ],
         ),
       ),
