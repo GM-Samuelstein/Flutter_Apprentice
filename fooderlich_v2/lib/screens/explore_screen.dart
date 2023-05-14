047d4c45cf458352c83e7ad65d068ad161604eb9
Ifeoluwa Ibitowa <ibitowaifeoluwa@yahoo.com>
2023-05-14 10:27:16 -0400

    Building Explore screen

--- fooderlich_v2/lib/screens/explore_screen.dart
+++ fooderlich_v2/lib/screens/explore_screen.dart
@@ -16,7 +16,18 @@ class ExploreScreen extends StatelessWidget {
       builder: (content, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           final recipes = snapshot.data?.todayRecipes ?? [];
-          return TodayRecipeListView(recipes: recipes);
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              const SizedBox(height:16),
+              // TODO: Replace this with FriendPostListView
+              Container(
+                height: 4000,
+                color:Colors.amber
+                ),
+            ],
+          );
         } else {
           return const Center(
             child: CircularProgressIndicator(),
