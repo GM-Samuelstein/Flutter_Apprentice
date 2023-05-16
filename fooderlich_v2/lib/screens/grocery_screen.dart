69800f6374ecd18f67f223eb11cb434bb6028a7c
Ifeoluwa Ibitowa <ibitowaifeoluwa@yahoo.com>
2023-05-16 04:43:53 -0400

    added consumer for grocery manager

--- fooderlich_v2/lib/screens/grocery_screen.dart
+++ fooderlich_v2/lib/screens/grocery_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'empty_grocery_screen.dart';
+import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Add a scaffold widget
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      body: buildGroceryScreen(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // TODO: Present GroceryItemScreen
+          
+        }
+      ),
+    );
   }
-  // TODO: Add buildGroceryScreen
+
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(builder: (context, manager, child) {
+      if (manager.groceryItems.isNotEmpty) {
+        // TODO: Add GroceryList Screen
+        return Container();
+      } else {
+        return const EmptyGroceryScreen();
+      }
+    });
+  }
 }
